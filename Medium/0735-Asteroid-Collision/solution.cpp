796a704a7383cf543e06faa2f23ff29616d02cc0
NITISH RANA <nitishrana2242006@gmail.com>
2026-06-23 10:45:19 +0530

    735. Asteroid Collision: add solution

---
+++ Medium/0735-Asteroid-Collision/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& arr) {
+        stack<int> st;
+        for(int i =0 ; i<arr.size() ; i++){
+            if(arr[i] > 0) st.push(arr[i]);
+            else{
+                while(!st.empty() && st.top() > 0 && st.top() < abs(arr[i])){
+                    st.pop();
+                }
+                if(!st.empty() && st.top() == abs(arr[i])){
+                    st.pop();
+                }
+                else if(st.empty() || st.top() < 0){
+                    st.push(arr[i]);
+                }
+            }
+        }
+        vector<int> ans(st.size());
+        for(int i = st.size() -1; i>=0 ;i--){
+            ans[i] = st.top();
+            st.pop();
+        }
+
+        return ans;
+    }
+};
