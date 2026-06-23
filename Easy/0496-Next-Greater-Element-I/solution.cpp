7e625d3946b932034571cd43425be743e2405177
NITISH RANA <nitishrana2242006@gmail.com>
2026-06-23 20:03:32 +0530

    496. Next Greater Element I: add solution

---
+++ Easy/0496-Next-Greater-Element-I/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> st;
+        unordered_map<int,int> mpp;
+
+        for(int i = nums2.size()-1 ; i>=0; i--){
+            while(!st.empty() && st.top() < nums2[i]){
+                st.pop();
+            }
+            mpp[nums2[i]] = st.empty()? -1 : st.top();
+            st.push(nums2[i]);
+        }
+
+        vector<int> ans;
+        for(auto i : nums1){
+            ans.push_back(mpp[i]);
+        }
+        return ans;
+    }
+};
