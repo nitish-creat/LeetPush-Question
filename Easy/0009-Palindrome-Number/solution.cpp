3e94e520ce1f6c6fb3cf04df3330c8173d89d94c
NITISH RANA <nitishrana2242006@gmail.com>
2026-06-23 10:46:26 +0530

    9. Palindrome Number: add solution

---
+++ Easy/0009-Palindrome-Number/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int original =x;
+        long long reversed=0;
+        while(x>0){
+            int digit=x%10;
+            reversed =reversed*10+digit;
+            x/=10;
+        }
+        if(original==reversed){
+            return true;
+
+        }
+        else {
+            return false;
+        }
+    }
+};
