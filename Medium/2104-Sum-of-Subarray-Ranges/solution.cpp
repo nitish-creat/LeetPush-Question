8592c0475de32a58c21e9c9d706ee01cb614f923
NITISH RANA <nitishrana2242006@gmail.com>
2026-06-28 16:16:09 +0530

    2104. Sum of Subarray Ranges: add solution

---
+++ Medium/2104-Sum-of-Subarray-Ranges/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        long long ans = 0;
+        for(int i= 0;i<nums.size();i++){
+            long long mini = nums[i];
+            long long maxi = nums[i];
+            for(int j = i+1 ;j<nums.size(); j++){
+                mini = min(mini , (long long)nums[j]);
+                maxi = max(maxi , (long long)nums[j]);
+                ans += maxi - mini;
+            }
+        }
+
+        return ans;
+    }
+};
