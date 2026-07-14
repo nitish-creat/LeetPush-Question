97166609d54113e5fa8e4be6e89d4467bbf5d7a9
NITISH RANA <nitishrana2242006@gmail.com>
2026-07-14 21:45:43 +0530

    605. Can Place Flowers: add solution

---
+++ Easy/0605-Can-Place-Flowers/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int size = flowerbed.size();
+        for (int i = 0; i< size; i++) {
+            if (flowerbed[i] == 0) {
+                bool left = (i == 0 || flowerbed[i - 1] == 0);
+                bool right = (i == size - 1 || flowerbed[i + 1] == 0);
+                if (left && right) {
+                    flowerbed[i] = 1;
+                    n--;
+                }
+            }
+        }
+
+        return n <= 0;
+    }
+};
