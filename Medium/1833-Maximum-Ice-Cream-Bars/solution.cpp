ef02e7b282a46326ae20f24a689760f3af09dd26
NITISH RANA <nitishrana2242006@gmail.com>
2026-08-24 19:01:02 +0530

    1833. Maximum Ice Cream Bars: add solution

---
+++ Medium/1833-Maximum-Ice-Cream-Bars/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        int count = 0;
+        sort(costs.begin(), costs.end());
+        for(int i = 0;i<costs.size(); i++){
+            if(coins >= costs[i]){
+                count++;
+                coins -= costs[i];
+            }
+            else break;
+        }
+
+        return count;
+    }
+};
