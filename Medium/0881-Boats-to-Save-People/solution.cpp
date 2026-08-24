80434c39ac21f54f707b4683becbefc022f02ec5
NITISH RANA <nitishrana2242006@gmail.com>
2026-08-24 23:16:59 +0530

    881. Boats to Save People: add solution

---
+++ Medium/0881-Boats-to-Save-People/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int left = 0;
+        int right = people.size()-1;
+        int boat = 0;
+        sort(people.begin(),people.end());
+        while(left <= right){
+            if (people[left]+people[right] <= limit){
+                left++;
+            }
+            right--;
+            boat++;
+        }
+        return boat;
+    }
+};
