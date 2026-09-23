51f954c8ce67c65d2402244311333267bf3ab4e8
NITISH RANA <nitishrana2242006@gmail.com>
2026-09-23 09:25:41 +0530

    98. Validate Binary Search Tree: add solution

---
+++ Medium/0098-Validate-Binary-Search-Tree/solution.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> arr;
+    void inorder(TreeNode* root){
+        if(!root) return;
+        inorder(root->left);
+        arr.push_back(root->val);
+        inorder(root->right);
+    }
+    bool isValidBST(TreeNode* root) {
+        inorder(root);
+        for(int i = 1; i<arr.size(); i++){
+            if(arr[i] <= arr[i-1]) return false;
+        }
+
+        return true;
+    }
+};
